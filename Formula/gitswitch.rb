1477496e112edd9f1edd81561596b48817169dee
omri <omri>
2024-06-16 23:37:27 +0300

    fix: create a virtual env for GitSwitch python

--- Formula/gitswitch.rb
+++ Formula/gitswitch.rb
@@ -1,30 +1,34 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gitswitch < Formula
+  include Language::Python::Virtualenv
+
   desc ""
   homepage ""
-  # url "https://github.com/target-ops/GitSwitch/archive/refs/tags/v0.1.0-alpha.tar.gz"
-  url "https://github.com/target-ops/GitSwitch.git",branch: "main", :using => :git
-  # sha256 "5d1dcd2a20729b965fa7d0a20373dce6c9fa623e2a9e7a6a8dbe2e9cf8046b2e"
+  url "https://github.com/target-ops/GitSwitch.git", branch: "main", :using => :git
   license ""
   version "0.1.0-alpha"
-  # depends_on "cmake" => :build
+  depends_on "python@3.9"
+
   def install
-    # Install the Python script
-    prefix.install Dir["src"]
-    chmod 0755, prefix/"src/main.py"
-    
+    # Create a virtual environment 
+    venv = virtualenv_create(libexec, "python3")
+
+    # Copy src directory and requirements.txt to libexec
+    FileUtils.cp_r Dir["src"], libexec
+    FileUtils.cp "requirements.txt", libexec
+
     # Install Python dependencies
-    system "pip3", "install", "-r", "requirements.txt"
+    system libexec/"bin/python3", "-m", "pip", "install", "--verbose", "--no-deps", "--no-binary=:all:", "--ignore-installed", "--no-compile", "-r", libexec/"requirements.txt"
     
-    # Create a symlink in bin
-    # bin.install_symlink "python3 #{prefix}/src/main.py" => "gitswitch4"
+    # Make the script executable
+    chmod 0755, libexec/"src/main.py"
+
+    # Create a wrapper script
     (bin/"gitswitch").write <<~EOS
-    #!/bin/bash
-    python3 #{prefix}/src/main.py "$@"
-  EOS
+      #!/bin/bash
+      #{libexec}/bin/python3 #{libexec}/src/main.py "$@"
+    EOS
   end
+
   test do
     system "false"
   end
